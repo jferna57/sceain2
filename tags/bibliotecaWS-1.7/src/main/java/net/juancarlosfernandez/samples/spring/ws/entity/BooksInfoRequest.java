eadbc7fcae800a979daaf790d762db91666620bc
jferna57 <jferna57@a9c68c7a-b26a-11dd-9d88-f59b771712dd>
2010-06-03 07:51:49 +0000

    [maven-release-plugin]  copy for tag bibliotecaWS-1.7

---
+++ tags/bibliotecaWS-1.7/src/main/java/net/juancarlosfernandez/samples/spring/ws/entity/BooksInfoRequest.java
@@ -0,0 +1,32 @@
+package net.juancarlosfernandez.samples.spring.ws.entity;
+
+/**
+ * Representacion de una peticion.
+ * @author Juan C. Fern�ndez
+ * @see http://www.juancarlosfernandez.net
+ */
+public class BooksInfoRequest {
+	private String categoria;
+	private String nivel;
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getCategoria() {
+		return categoria;
+	}
+	/**
+	 * 
+	 * @param categoria
+	 */
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getNivel() {
+		return nivel;
+	}
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
+}
