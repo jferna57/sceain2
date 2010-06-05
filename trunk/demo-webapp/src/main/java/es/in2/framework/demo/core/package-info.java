39cbc1657527565910ce8e9f3b09e84f3ede2cb0
jferna57 <jferna57@a9c68c7a-b26a-11dd-9d88-f59b771712dd>
2010-06-05 19:28:09 +0000

    Refactor a es.in2.framework

---
+++ trunk/demo-webapp/src/main/java/es/in2/framework/demo/core/package-info.java
@@ -0,0 +1,51 @@
+/*
+ * Demoiselle Framework
+ * Copyright (c) 2009 Serpro and other contributors as indicated
+ * by the @author tag. See the copyright.txt in the distribution for a
+ * full listing of contributors.
+ *
+ * Demoiselle Framework is an open source Java EE library designed to accelerate
+ * the development of transactional database Web applications.
+ *
+ * Demoiselle Framework is released under the terms of the LGPL license 3
+ * http://www.gnu.org/licenses/lgpl.html  LGPL License 3
+ *
+ * This file is part of Demoiselle Framework.
+ *
+ * Demoiselle Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License 3 as published by
+ * the Free Software Foundation.
+ *
+ * Demoiselle Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Demoiselle Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+
+/** 
+ * <p>
+ * <b>CORE Logical Module</b><br/>
+ * This module contains the specification group that is the framework structural
+ * base. It enables standardization, extension and integration between the tiers 
+ * of applications based on it.
+ * </p>
+ * <p>
+ * Subpackages:
+ * <ul>
+ * <li>action   
+ * <li>bean
+ * <li>context
+ * <li>exception
+ * <li>layer
+ * <li>layer.integration
+ * <li>message
+ * <li>security
+ * <li>transaction
+ * </ul>
+ * </p>
+ * @author CETEC/CTJEE 
+ */
+package es.in2.framework.demo.core;
