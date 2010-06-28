917ef705c00bbe0f8ba99bfe4e37241a45910269
jferna57 <jferna57@a9c68c7a-b26a-11dd-9d88-f59b771712dd>
2010-06-28 06:58:48 +0000

    [maven-release-plugin]  copy for tag demo-webapp-0.0.30

--- demo-webapp/src/test/java/es/in2/framework/demo/persistence/hibernate/HibernateGenericDAOStub.java
+++
@@ -1,9 +0,0 @@
-package es.in2.framework.demo.persistence.hibernate;
-
-/**
- * @author CETEC/CTJEE
- */
-public class HibernateGenericDAOStub<A extends Professor> extends
-		HibernateGenericDAO<A> {
-
-}
